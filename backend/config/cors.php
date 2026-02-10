47e6390ad4d886bd9f6ce3937c47a8b126dab8b1
Naufal Firman Dhani <naufal@example.com>
2026-02-10 20:02:11 +0700

    last

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://aksamedia-intern-tawny.vercel.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
